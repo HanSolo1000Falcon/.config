379183d1a893806770f2f5d2570b460aa069445e
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-05-08 22:01:31 +0200

    Commited with fsysutils.

--- fish/config.fish
+++ fish/config.fish
@@ -7,14 +7,7 @@ alias larp-larp-larp-sahur "fastfetch --logo ~/.config/fastfetch/larp-larp-larp-
 if status is-interactive
     set fish_greeting
     clear
-
-    set num (random 1 10)
-
-    if test $num -eq 5
-        larp-larp-larp-sahur
-    else
-        fastfetch
-    end
+    fastfetch
 end
 
 # init
