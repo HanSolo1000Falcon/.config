d15b0455e5d797022b9d3473f5d617271efd42cc
HanSolo1000Falcon <HanSolo1000Falcon.Business@gmail.com>
2026-06-14 21:57:36 +0200

    Commited with fsysutils.

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,16 @@
-# ~/.config/fish/config.fish
-
-# aliases
 alias fastfetch ~/.config/fish/fastfetchwrapper.sh
 alias cls clear
 alias clearfetch "clear;fastfetch"
 alias clf "clear;fastfetch"
 
-if status is-interactive
-    set fish_greeting
-    clear
-    fastfetch
+set fish_greeting
+clear
+
+if not set -q ZED_TERM
+	fastfetch
+	starship init fish | source
 end
 
-# init
-starship init fish | source
-fnm env --use-on-cd --shell fish | source
 fish_add_path ~/.fsysutils
 fish_add_path ~/.tarball-installations/zen
 fish_add_path ~/.local/bin
