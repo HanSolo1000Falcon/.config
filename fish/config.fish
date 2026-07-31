5ccc2300ee77f593343647dbbb34c5c7a0694e84
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-07-31 21:52:13 +0200

    Update dotfiles with fsysutils

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,5 @@
 set -g fish_key_bindings fish_vi_key_bindings
 
-# alias fastfetch ~/.config/fish/fastfetchwrapper.sh
-alias supac ~/.config/fish/supac.sh
 alias cls clear
 alias clearfetch "clear;fastfetch"
 alias clf "clear;fastfetch"
@@ -15,6 +13,5 @@ end
 
 starship init fish | source
 
-fish_add_path ~/.fsysutils
 fish_add_path ~/.tarball-installations/zen
 fish_add_path ~/.local/bin
